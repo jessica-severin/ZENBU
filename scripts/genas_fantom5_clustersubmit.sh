29218c8d683c50d6c28385eba9af869b960221f8
jessica-severin <jessica.severin@gmail.com>
2020-01-28 14:36:47 +0900

    replaced the fake password with <pass> to make it clearer that it needs to be changed

--- scripts/genas_fantom5_clustersubmit.sh
+++ scripts/genas_fantom5_clustersubmit.sh
@@ -2,4 +2,4 @@
 export EEDB_ROOT="/zenbu/src/ZENBU_2.302"
 hostname
 echo $1
-/usr/bin/perl /zenbu/src/ZENBU_2.302/scripts/eedb_create_OSCFileDB.pl -LSA -single_tagmap -file $1 -keywords "genas autoload fantom5" -builddir /work/scratch -deploydir severin@osc-fs.gsc.riken.jp:/eeDB2/dbs/genas_autoload/fantom5 -registry "mysql://severin:sevjes753@osc-mysql.gsc.riken.jp/eeDB_test_autoregistry"
+/usr/bin/perl /zenbu/src/ZENBU_2.302/scripts/eedb_create_OSCFileDB.pl -LSA -single_tagmap -file $1 -keywords "genas autoload fantom5" -builddir /work/scratch -deploydir severin@osc-fs.gsc.riken.jp:/eeDB2/dbs/genas_autoload/fantom5 -registry "mysql://severin:<pass>@osc-mysql.gsc.riken.jp/eeDB_test_autoregistry"
