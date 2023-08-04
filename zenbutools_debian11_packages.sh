acd71b812a321b84353bd1aceec7739f2562f030
Jessica Severin <severin@zenbu-tmp.gsc.riken.jp>
2023-08-04 11:34:47 +0900

    updated for mariadb and some perl packages

--- zenbutools_debian11_packages.sh
+++ zenbutools_debian11_packages.sh
@@ -4,12 +4,14 @@ apt install openssl samtools sqlite3 uuid-runtime wget
 
 apt install  bzip2 cmake cvs expat g++ gcc git make 
 
-apt install default-mysql-client default-libmysqlclient-dev  
+apt install default-mysql-client libmariadbclient-dev  libmysql++-dev
 
 apt install libboost-dev libbz2-dev libcrypto++-dev libexpat1-dev libcurl4-gnutls-dev
 
 apt install libfcgi-dev libhts-dev liblzma-dev libssl-dev zlib1g-dev libncurses-dev libsqlite3-dev 
 
-apt install libmysql++-dev  libapache2-mod-fcgid 
+apt install libapache2-mod-fcgid 
 
-apt install perl-doc libcache-perl libcgi-application-perl libcgi-fast-perl libclass-dbi-mysql-perl  libclass-dbi-sqlite-perl libcrypt-openssl-bignum-perl libdata-uuid-perl libio-all-lwp-perl liblwp-authen-oauth-perl liblwpx-paranoidagent-perl libnet-openid-common-perl libnet-openid-consumer-perl libnet-oping-perl libswitch-perl libxml-treepp-perl libyaml-perl 
+apt install perl-doc libcache-perl libcgi-application-perl libcgi-fast-perl libclass-dbi-sqlite-perl libcrypt-openssl-bignum-perl libdata-uuid-perl libio-all-lwp-perl liblwp-authen-oauth-perl liblwpx-paranoidagent-perl libnet-openid-common-perl libnet-openid-consumer-perl libnet-oping-perl libswitch-perl libxml-treepp-perl libyaml-perl 
+
+apt install libclass-dbi-mysql-perl 
