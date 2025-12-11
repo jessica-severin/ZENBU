410b30be8efbb25f0c0d217e537b2d05f2998bfb
Jessica Severin <jessica.severin@riken.jp>
2025-12-11 11:16:59 +0900

    updated debian11 packages for mariadb

--- zenbutools_debian11_packages.sh
+++ zenbutools_debian11_packages.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
 
-apt install openssl samtools sqlite3 uuid-runtime wget
-
-apt install  bzip2 cmake cvs expat g++ gcc git make 
-
-apt install default-mysql-client libmariadbclient-dev  libmysql++-dev
-
-apt install libboost-dev libbz2-dev libcrypto++-dev libexpat1-dev libcurl4-gnutls-dev
-
-apt install libfcgi-dev libhts-dev liblzma-dev libssl-dev zlib1g-dev libncurses-dev libsqlite3-dev 
-
-apt install libapache2-mod-fcgid 
-
-apt install perl-doc libcache-perl libcgi-application-perl libcgi-fast-perl libclass-dbi-sqlite-perl libcrypt-openssl-bignum-perl libdata-uuid-perl libio-all-lwp-perl liblwp-authen-oauth-perl liblwpx-paranoidagent-perl libnet-openid-common-perl libnet-openid-consumer-perl libnet-oping-perl libswitch-perl libxml-treepp-perl libyaml-perl 
-
-apt install libclass-dbi-mysql-perl 
+apt install openssl samtools sqlite3 uuid-runtime wget \
+	bzip2 cmake cvs expat g++ gcc git make  \
+	mariadb-client mariadb-common default-libmysqlclient-dev default-mysql-client \
+	libmariadb-dev-compat libmariadb-dev libmysql++-dev \
+	libboost-dev libbz2-dev libcrypto++-dev libexpat1-dev libcurl4-gnutls-dev \
+	libfcgi-dev libhts-dev liblzma-dev libssl-dev zlib1g-dev libncurses-dev libsqlite3-dev  \
+	libapache2-mod-fcgid  \
+	perl-doc libcache-perl libcgi-application-perl libcgi-fast-perl libclass-dbi-sqlite-perl  \
+	libcrypt-openssl-bignum-perl libdata-uuid-perl libio-all-lwp-perl liblwp-authen-oauth-perl  \
+	liblwpx-paranoidagent-perl libnet-openid-common-perl libnet-openid-consumer-perl libnet-oping-perl  \
+	libswitch-perl libxml-treepp-perl libyaml-perl libclass-dbi-mysql-perl
